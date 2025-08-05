9e9d1e3820ee7c147dffdd4f4601fea04f6c2b18
Pradeep Selakoti <pradeepselakoti@gmail.com>
2025-08-05 17:32:58 +0530

    completed signin/sign--up

--- components/FormField.tsx
+++ components/FormField.tsx
@@ -1,23 +1,41 @@
+import {
+  FormControl,
+  FormDescription,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Controller, FieldValues,Control, Path } from "react-hook-form";
 
-import { FormControl,FormDescription, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+interface FormFieldProps<T extends FieldValues>{
+    control: Control<T>;
+    name: Path<T>;
+    label : string;
+    placeholder?:string;
+    type?: 'text' | 'email' | 'password' | 'file'
 
-const FormField = () => (
-     <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-)
-export default FormField
+}
+
+const FormField = <T extends FieldValues>({control, name, label, placeholder, type="text"}:FormFieldProps<T>) => (
+  <Controller
+    control={control}
+    name={name}
+    render={({ field }) => (
+      <FormItem>
+        <FormLabel className="label">{label}</FormLabel>
+        <FormControl>
+
+          <Input 
+          className = "input" 
+          placeholder={placeholder} 
+          type={type}
+          {...field} />
+
+        </FormControl>
+        <FormMessage />
+      </FormItem>
+    )}
+  />
+);
+export default FormField;
