37aa735fd0e5fc1ad90d798dd8fdbbd3f580480d
Pradeep Selakoti <pradeepselakoti@gmail.com>
2025-08-06 17:29:51 +0530

    Home Page Completed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,52 @@
 import React from 'react'
+import Link from 'next/link'
+import { Button } from "@/components/ui/button";
+import Image from 'next/image';
+import { dummyInterviews } from '@/constants';
+import InterviewCard from '@/components/InterviewCard';
 
 const page = () => {
   return (
-    <div>Home Page</div>
+    <>
+      <section className='card-cta'>
+        <div className='flex flex-col gap-6 max-w-lg'>
+          <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+       
+        <p className='text-lg'>
+          Practice on real interview question & get instant Feedback
+        </p>
+
+        <Button asChild className='btn-primary max-sm:w-full'>
+          <Link href="/interview">Start an Interview</Link>
+        </Button>
+         </div>
+
+        <Image src="/robot.png" alt="robo-dude" width={400} height={400} className="max-sm:hidden"/>
+
+      </section>
+
+      <section className='flex flex-col gap-6 mt-8'>
+        <h2>Your Interview</h2>
+
+        <div className='interview-section'>
+          {dummyInterviews.map((interview)=>(
+            <InterviewCard {...interview}/>
+          ))}
+        </div>
+      </section>
+
+      <section className='flex flex-col gap-6 mt-6'>
+        <h2>Take an Interview</h2>
+        
+        <div className='interviews-section'>
+          {dummyInterviews.map((interview)=>(
+            <InterviewCard {...interview} key={interview.id}/>
+          ))}
+
+          {/* <p>You haven$apos't taken any interviews yet</p> */}
+        </div>
+      </section>
+    </>
   )
 }
 
